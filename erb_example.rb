41bf847a48f1e5a924017e2b689bdcaf28b472dd
scheller <scheller.sanchez@gmail.com>
2012-09-01 17:53:39 -1000

    added 3 examples to erb_example

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,27 @@
 require 'erb'
-
 x = 42
+
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+puts ""
+
+animals = ["cat", "dog", "fish", "bird"]
+animals.each do |animal|
+  phrase = "i love my #{animal}"
+  template = ERB.new "<%= phrase %>"
+  puts template.result(binding)
+end
+puts ""
+
+template = ERB.new "Animals.count is <%= animals.count %>"
+puts template.result(binding)
+puts""
+
+##animals.map do |animal|
+##  animal.upcase
+##end
+
+animals.each do |a|
+  template = ERB.new "i love my <%= a.upcase %> "
+  puts template.result(binding)
+end
